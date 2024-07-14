f3f2bd37f35a4557b3c1cd4c41b28c930d37f1e5
Ayushmaan Das <dasayush5maan@gmail.com>
2024-07-14 18:38:20 +0530

    prompt-to-code implementation

---
+++ prompt2code.py
@@ -0,0 +1,82 @@
+import torch
+import spacy
+import pickle
+from tokenize import tokenize, untokenize
+from torchtext.data import Field
+from colorama import Fore, Style, init
+from prompt_model import Encoder, EncoderLayer, PositionwiseFeedforwardLayer, MultiHeadAttentionLayer, Decoder, DecoderLayer, Seq2Seq
+
+
+try:
+    model = torch.load('conversational-ai-model-main.pt',
+                       map_location=torch.device('cpu'))
+    print(f"{Fore.LIGHTGREEN_EX}Model fetched successfully \n{Style.RESET_ALL}")
+except Exception as e:
+    print(f"{Fore.RED}Error in fetching the model : {e} \n{Style.RESET_ALL}")
+
+SRC = Field(tokenize=lambda x: x.split(),
+            init_token='<sos>',
+            eos_token='<eos>',
+            lower=True)
+
+TRG = Field(tokenize=lambda x: x.split(),
+            init_token='<sos>',
+            eos_token='<eos>',
+            lower=True)
+
+with open('source_vocab.pkl', 'rb') as f:
+    src_vocab = pickle.load(f)
+
+with open('target_vocab.pkl', 'rb') as f:
+    trg_vocab = pickle.load(f)
+
+SRC.vocab = src_vocab
+TRG.vocab = trg_vocab
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def translate_sentence(sentence, src_field, trg_field, model, device, max_len=50000):
+    model.eval()
+    if isinstance(sentence, str):
+        nlp = spacy.load('en')
+        tokens = [token.text.lower() for token in nlp(sentence)]
+    else:
+        tokens = [token.lower() for token in sentence]
+
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+    src_mask = model.make_src_mask(src_tensor)
+
+    with torch.no_grad():
+        enc_src = model.encoder(src_tensor, src_mask)
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+
+    for _ in range(max_len):
+        trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+        trg_mask = model.make_trg_mask(trg_tensor)
+        with torch.no_grad():
+            output, attention = model.decoder(
+                trg_tensor, enc_src, trg_mask, src_mask)
+        pred_token = output.argmax(2)[:, -1].item()
+        trg_indexes.append(pred_token)
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+    return trg_tokens[1:], attention
+
+
+def eng_to_python(src):
+    src = src.split(" ")
+    translation, attention = translate_sentence(src, SRC, TRG, model, device)
+
+    print(f'predicted trg: \n')
+    print(untokenize(translation[:-1]).decode('utf-8'))
+
+
+if __name__ == "__main__":
+    src = "program to print even numbers"
+    eng_to_python(src)
